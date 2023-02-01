9099f4cc2ef499096f97ba1cf9815277e87fc3d2
ssu-humane <80903024+ssu-humane@users.noreply.github.com>
2023-02-01 17:31:47 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,17 +1,14 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
 from tqdm.auto import tqdm
 from torch.utils.data import Dataset, DataLoader
 import torch
 
     
 class QuoteCSE_Dataset(Dataset):
-    def __init__(self, args, original_texts, positive_texts, negative_texts, label):
+    def __init__(self, args, original_texts, positive_texts, negative_texts):
         self.tokenizer = args.tokenizer
         self.org = []
         self.pos = []
         self.neg = []
-        self.label = []
         self.max_len = args.max_len
         assert len(original_texts) == len(positive_texts)
 
@@ -38,38 +35,13 @@ class QuoteCSE_Dataset(Dataset):
             self.org.append(org_input)
             self.pos.append(pos_input)
             self.neg.append(neg_input)
-            self.label.append(label[idx])
 
     def __len__(self):
         return len(self.org)
 
     def __getitem__(self, idx):
-        return self.org[idx], self.pos[idx], self.neg[idx], torch.tensor(self.label[idx], dtype=torch.long)
+        return self.org[idx], self.pos[idx], self.neg[idx]
     
-
-    
-def make_dataset(args):
-    df_modified = pd.read_pickle(args.MODIFIED_DATA_PATH)
-    df_contextomized = pd.read_pickle(args.CONTEXTOMIZED_DATA_PATH)
-
-    df_modified['title_quote'] = df_modified['title_quote'].map(lambda x:x[0])
-    df_contextomized['title_quote'] = df_contextomized['title_quote'].map(lambda x:x[0])
-
-    # modified: 0, contextomized: 1
-    df_modified['label'] = 0
-    df_contextomized['label'] = 1
-
-    train_df_contextomized, test_df_contextomized = train_test_split(df_contextomized, test_size=0.2, random_state=args.seed)
-    valid_df_contextomized, test_df_contextomized = train_test_split(test_df_contextomized, test_size=0.5, random_state=args.seed)
-
-    train_df_modified, test_df_modified = train_test_split(df_modified, test_size=0.2, random_state=args.seed)
-    valid_df_modified, test_df_modified = train_test_split(test_df_modified, test_size=0.5, random_state=args.seed)
-
-    train_df = pd.concat([train_df_contextomized, train_df_modified])
-    valid_df = pd.concat([valid_df_contextomized, valid_df_modified])
-    test_df = pd.concat([test_df_contextomized, test_df_modified])
-
-    return train_df, valid_df, test_df
 
 
 
@@ -79,7 +51,6 @@ def create_data_loader(args, df, shuffle, drop_last):
         original_texts = df.title_quote.to_numpy(),
         positive_texts = df.positive_sentence.to_numpy(),
         negative_texts = df.negative_sentence.to_numpy(),
-        label = df.label.to_numpy(),
     )
 
     return DataLoader(
